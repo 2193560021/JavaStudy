13e8626733f69ea00dd8a7c3169eb90134ff05a5
liuyuyang <2193560021@qq.com>
2024-03-22 15:23:27 +0800

    复习Java字符串和封装类

--- JavaStudy/Method/src/com/lyy/chapter05/java04_String_2.java
+++ JavaStudy/Method/src/com/lyy/chapter05/java04_String_2.java
@@ -3,7 +3,17 @@ package com.lyy.chapter05;
 public class java04_String_2 {
     public static void main(String[] args) {
 
-        String name = 1 + "123" + 2;
-        System.out.println(name);
+        String s = "";
+        for (int i = 0;i < 100;i++){
+            s += i;
+        }
+        System.out.println(s);
+
+        StringBuilder s1 = new StringBuilder();
+        for (int i = 0;i < 100;i++){
+            s1.append(i);
+        }
+        System.out.println(s1);
+        System.out.println(s1.reverse());
     }
 }
---
+++ JavaStudy/Method/src/com/lyy/chapter05/java05_Pbject.java
@@ -0,0 +1,22 @@
+package com.lyy.chapter05;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+public class java05_Pbject {
+    public static void main(String[] args) {
+
+//        Byte b= null;
+//        Short s =null;
+//        Integer i = null;
+//        Long lon = null;
+//        Float f = null;
+//        Double d = null;
+//        Character c = null;
+//        Boolean bln = null;
+
+        int i =10;
+        Integer i1 = i;
+        System.out.println(i1);
+
+    }
+}
