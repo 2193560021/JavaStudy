5fe85ecb58369f7be0da28489ff743e1b251ce08
liuyuyang <2193560021@qq.com>
2024-09-19 11:10:07 +0800

    学习Maven_1

---
+++ Maven/maven_web/src/main/java/com/atguigu/maven/pojo/Student.java
@@ -0,0 +1,8 @@
+package com.atguigu.maven.pojo;
+
+public class Student {
+
+    public void sayHello(){
+
+    }
+}
---
+++ Maven/maven_web/src/test/java/com/atguigu/maven/test/DruidTest.java
@@ -0,0 +1,10 @@
+package com.atguigu.maven.test;
+
+import org.junit.jupiter.api.Test;
+
+public class DruidTest {
+    @Test
+    public void testDruid(){
+
+    }
+}
