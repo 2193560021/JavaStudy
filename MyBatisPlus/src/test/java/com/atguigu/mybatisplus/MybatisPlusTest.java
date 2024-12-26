e5a2abc1e2b998a9d5a8d03ffac5c3b9893f9fba
liuyuyang <2193560021@qq.com>
2024-12-26 21:22:32 +0800

    Mybatis_plus_2

---
+++ MyBatisPlus/src/test/java/com/atguigu/mybatisplus/MybatisPlusTest.java
@@ -0,0 +1,56 @@
+package com.atguigu.mybatisplus;
+
+import com.atguigu.mybatisplus.mapper.UserMapper;
+import com.atguigu.mybatisplus.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+public class MybatisPlusTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelectList(){
+        List<User> users = userMapper.selectList(null);
+
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testInsert(){
+        User user = new User();
+        user.setName("刘宇阳");
+        user.setAge(23);
+        user.setEmail("2193@qq.com");
+        int insert = userMapper.insert(user);
+
+        System.out.println(insert);
+
+    }
+
+    @Test
+    public void delById(){
+//        int i = userMapper.deleteById(1872268618859810817L);
+//        System.out.println(i);
+
+//        Map<String,Object> map = new HashMap<>();
+//
+//        map.put("name","刘宇阳");
+//        map.put("age",23);
+//
+//        userMapper.deleteByMap(map);
+
+        List<Long> list = Arrays.asList(1L, 2L, 3L);
+        int i = userMapper.deleteBatchIds(list);
+        System.out.println(i);
+    }
+
+}
