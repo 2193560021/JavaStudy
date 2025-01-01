47cc9475b3d44d24093892e4d58a0f0380440b17
liuyuyang <2193560021@qq.com>
2025-01-01 19:54:13 +0800

    Mybatis_plus_4

--- MyBatisPlus/src/main/java/com/atguigu/mybatisplus/pojo/User.java
+++ MyBatisPlus/src/main/java/com/atguigu/mybatisplus/pojo/User.java
@@ -1,17 +1,23 @@
 package com.atguigu.mybatisplus.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.*;
 
 @Data
-@TableName("use")
+@TableName("user")
 public class User {
     private Long id;
 
+//    @TableField("name")
     private String name;
 
     private Integer age;
 
     private String email;
+
+    @TableLogic
+    private Integer isDeleted;
 
 }
---
+++ MyBatisPlus/src/test/java/com/atguigu/mybatisplus/MybatisPlusWrapperTest.java
@@ -0,0 +1,92 @@
+package com.atguigu.mybatisplus;
+
+import com.atguigu.mybatisplus.mapper.UserMapper;
+import com.atguigu.mybatisplus.pojo.User;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+public class MybatisPlusWrapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void test01(){
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name","a")
+                        .between("age",20,30)
+                                .isNotNull("email");
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void test02(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("age").orderByAsc("id");
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+
+    }
+
+    @Test
+    public void test03(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.isNull("email");
+        int delete = userMapper.delete(queryWrapper);
+        System.out.println(delete);
+
+
+
+    }
+
+    @Test
+    public void test04(){
+        //将（年龄大于20并且用户名中包含有a）或邮箱为null的用户信息修改
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.gt("age",20)
+                        .like("name","a")
+                                .or()
+                                        .isNull("email");
+        User user = new User();
+        user.setName("小明");
+        user.setEmail("XiaoMing@gamil.com");
+        int update = userMapper.update(user, queryWrapper);
+        System.out.println(update);
+    }
+
+    @Test
+    public void test05(){
+        //将用户名中包含有a并且（年龄大于20或邮箱为null）的用户信息修改
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like("name","a")
+                .and(i -> i.gt("age",20).or().isNull("email"));
+        User user = new User();
+        user.setName("小明");
+        user.setEmail("XiaoMing@gamil.com");
+        int update = userMapper.update(user, queryWrapper);
+        System.out.println(update);
+    }
+
+
+    @Test
+    public void test06(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("name","age","email");
+        List<Map<String, Object>> maps = userMapper.selectMaps(queryWrapper);
+        maps.forEach(System.out::println);
+    }
+
+    @Test
+    public void test07(){
+
+    }
+
+}
