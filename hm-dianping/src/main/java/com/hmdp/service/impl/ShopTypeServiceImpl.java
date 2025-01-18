1e3c60a9ce9e4e4d33a6c2bb8e448a0f20a89a07
liuyuyang <2193560021@qq.com>
2025-01-18 18:27:35 +0800

    店铺查询缓存

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,29 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.Shop;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 /**
  * <p>
@@ -17,4 +36,49 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryList() {
+
+
+        //1.从redis查询商铺缓存
+
+        String shopTypeJson = stringRedisTemplate.opsForValue().get("cache:shopType");
+
+        System.out.println("111shopTypeJson = " + shopTypeJson);
+
+        //2.判断是否存在
+        if (StrUtil.isNotBlank(shopTypeJson)) {
+            //3.存在，直接返回
+            assert shopTypeJson != null;
+//            shopTypeJson = shopTypeJson.substring(1, shopTypeJson.length() - 1 );
+            System.out.println("222shopTypeJson = " + shopTypeJson);
+
+            List<ShopType> shopType = new ArrayList<>();
+
+            JSONArray objects = JSONUtil.parseArray(shopTypeJson);
+
+//            shopType.addAll(JSONUtil.parseArray(shopTypeJson, ShopType.class)));
+            System.out.println("objects = " + objects);
+            return Result.ok(objects);
+        }
+
+        //4.不存在，查询数据库
+        QueryWrapper<ShopType> queryShopType = new QueryWrapper<>();
+        queryShopType.getEntity();
+        List<ShopType> shopTypeList = list(queryShopType);
+
+        //5.不存在，返回错误
+        if (shopTypeList == null) {
+            return Result.fail("店铺列表查询错误");
+        }
+
+        //6.存在，写入redis
+        stringRedisTemplate.opsForValue().set("cache:shopType", JSONUtil.toJsonStr(shopTypeList));
+        //7.返回
+        return Result.ok(shopTypeList);
+
+    }
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,20 +1,31 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.bean.copier.CopyOptions;
+import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
 import com.hmdp.utils.SystemConstants;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.*;
 import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
@@ -29,6 +40,8 @@ import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public Result sendCode(String phone, HttpSession session) {
@@ -44,9 +57,11 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
         String code = RandomUtil.randomNumbers(6);
 
-        //4.保存至session
+        //4.保存至redis
 
-        session.setAttribute("code",code);
+//        session.setAttribute("code",code);
+
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY + phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
 
         //5.发送验证码
         log.debug("成功发送短信验证码:{}",code);
@@ -66,10 +81,10 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             //2.返回错误信息
             return Result.fail("手机号格式错误!");
         }
-        //2.校验验证码
-        Object cache_code = session.getAttribute("code");
+        //从redis获取验证码
+        String cache_code = stringRedisTemplate.opsForValue().get(LOGIN_CODE_KEY + phone);
         String code = loginForm.getCode();
-        if(cache_code == null || !cache_code.toString().equals(code)){
+        if(cache_code == null || !cache_code.equals(code)){
             //3.不一致，报错
             return Result.fail("验证码错误，请重新发送验证码");
         }
@@ -81,9 +96,27 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
             //6.不存在，创新新用户并保存
             user = createUserWithPhone(phone);
         }
-        //7.保存用户信息至session
-        session.setAttribute("user",user);
-        return Result.ok();
+
+
+        //7.保存用户信息至redis
+        //7.1随机生成token，作为登陆令牌
+
+        String token = UUID.randomUUID().toString(true);
+        //7.2将User对象转为Hash存储
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        Map<String, Object> userMap = BeanUtil.beanToMap(userDTO,new HashMap<>(),
+                CopyOptions.create()
+                        .setIgnoreNullValue(true)
+                        .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+        //7.3存储
+        String redis_token = LOGIN_USER_KEY + token;
+        stringRedisTemplate.opsForHash().putAll(redis_token, userMap);
+        //7.4设置有效期
+        stringRedisTemplate.expire(redis_token, LOGIN_USER_TTL, TimeUnit.MINUTES);
+
+
+        //8.返回客户端
+        return Result.ok(token);
     }
 
     private User createUserWithPhone(String phone) {
