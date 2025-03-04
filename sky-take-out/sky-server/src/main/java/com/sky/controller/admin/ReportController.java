398f96f9659b9c421d6480ae6997147629418757
liuyuyang <2193560021@qq.com>
2025-03-04 17:48:26 +0800

    苍穹外卖day10_营业额统计

---
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -0,0 +1,45 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.service.ReportService;
+import com.sky.utils.AliOssUtil;
+import com.sky.vo.TurnoverReportVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * 通用controller
+ */
+@RestController
+@RequestMapping("/admin/report")
+@Slf4j
+@Api(tags = "统计接口")
+public class ReportController {
+
+    @Autowired
+    public ReportService reportService;
+
+
+    @GetMapping("/turnoverStatistics")
+    @ApiOperation("营业额统计")
+    public Result<TurnoverReportVO> turnoverStatistics(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        log.info("营业额统计:{},{}", begin, end);
+        return Result.success(reportService.getTurnoverStatistics(begin, end));
+
+    }
+}
---
+++ sky-take-out/sky-server/src/main/java/com/sky/service/ReportService.java
@@ -0,0 +1,12 @@
+package com.sky.service;
+
+import com.sky.vo.TurnoverReportVO;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public interface ReportService {
+
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
+}
