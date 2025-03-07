48f695a9272c2102325fb065acf1285a44850b6e
liuyuyang <2193560021@qq.com>
2025-03-07 10:01:48 +0800

    begin:Java Data Structure1

--- DataStructure/src/main/java/com/lyy/Algorithm/MergeSort.java
+++ DataStructure/src/main/java/com/lyy/Algorithm/MergeSort.java
@@ -1,7 +1,44 @@
 package com.lyy.Algorithm;
 
+import java.util.Scanner;
+
 public class MergeSort {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+//        int k = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        MergeSort(arr,0,n - 1);
+//        System.out.println(arr[k - 1]);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void MergeSort(int[] arr, int l, int r){
+        if(l >= r) return;
+
+        int mid = (l + r) >> 1;
+        int[] temp = new int[r - l + 1];
+        MergeSort(arr,l,mid);
+        MergeSort(arr,mid + 1,r);
+        int i = l;
+        int j = mid + 1;
+        int k = 0;
+        while(i <= mid && j <= r) {
+            if(arr[i] <= arr[j]) temp[k++] = arr[i++];
+            else temp[k++] = arr[j++];
+        }
+        while(i <= mid) temp[k++] = arr[i++];
+        while(j <= r) temp[k++] = arr[j++];
+        for (int m = l,n = 0; m <= r; m++,n++) {
+            arr[m] = temp[n];
+        }
 
     }
 }
--- DataStructure/src/main/java/com/lyy/Algorithm/SortCollections.java
+++ DataStructure/src/main/java/com/lyy/Algorithm/SortCollections.java
@@ -1,2 +1,45 @@
-package com.lyy.Algorithm;public class SortCollections {
+package com.lyy.Algorithm;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class SortCollections {
+    public static void main(String[] args) throws Exception {
+        //Scanner scanner = new Scanner(System.in);
+        //int n = scanner.nextInt();
+        //arr[i] = scanner.nextInt();
+
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        String[] res = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(res[i]);
+        }
+
+
+        //归并排序
+        MergeSort(arr,0,n - 1);
+
+        //快速排序
+        QuickSort(arr,0,n - 1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void MergeSort(int[] arr, int l, int r){
+        if(l >= r) return;
+    }
+
+
+    public static void QuickSort(int[] arr, int l, int r){
+
+    }
 }
