5544d38469cdb046f74ec2b4e8141c728e5b5e28
liuyuyang <2193560021@qq.com>
2025-09-06 01:27:30 +0800

    刷力扣——每日温度，使用栈来操作

--- DataStructure/src/main/java/com/lyy/Array/test01.java
+++ DataStructure/src/main/java/com/lyy/Array/test01.java
@@ -1,6 +1,6 @@
 package com.lyy.Array;
 
-public class test01 {
+public class test01{
     public static void main(String[] args) {
         int[] a1 = new int[]{1, 2, 3, 4};
 
--- DataStructure/src/main/java/com/lyy/LeetCode/no739.java
+++ DataStructure/src/main/java/com/lyy/LeetCode/no739.java
@@ -1,20 +1,47 @@
 package com.lyy.LeetCode;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Hashtable;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.StampedLock;
 
 public class no739 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Lock lock = new ReentrantLock();
+        Semaphore semaphore = new Semaphore(1);
+        StampedLock stampedLock = new StampedLock();
+        new ArrayBlockingQueue<Integer>(10);
+        LockSupport.park();
+        new Hashtable<Integer, Integer>();
+        try {
+            lock.lock();
+        } finally {
+            lock.unlock();
+        }
+        LockSupport.unpark(Thread.currentThread());
+        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        threadMXBean.findDeadlockedThreads();
         int[] temperatures = {73,74,75,71,69,72,76,73};
         int[] dailied = dailyTemperatures(temperatures);
         for (int i : dailied) {
             System.out.println("i = " + i);
         }
 
+        while (true) {
+            int i = 1;
+        }
     }
 
     public static int[] dailyTemperatures(int[] temperatures) {
+
         int len = temperatures.length;
         int[] ans = new int[len];
         Deque<Integer> stack = new ArrayDeque<>();
